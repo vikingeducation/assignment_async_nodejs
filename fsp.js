5852ac3c8740e8260d5335a24ee5d96184692cc2
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-19 10:35:28 +0000

    Wrap fs.readFile, writeFile, appendFile in Promises. Fsp module

--- fsp.js
+++ fsp.js
@@ -1,7 +1,43 @@
 let fs = require("fs");
 
+function promisify(fn) {
+    return new Promise(function (resolve, reject) {
+        fn.call(null);
+    });
+}
+
+
 let fsp = {
-    
+    "readFile" : function (path) {
+        return new Promise(function (resolve, reject) {
+            fs.readFile(path, "utf8", function (err, data) {
+                if (err) {
+                    reject(err); //Rejected promise will terminate the function
+                }
+                resolve(data);
+            });
+        });
+    },
+    "writeFile" : function (path, text) {
+        return new Promise(function (resolve, reject) {
+            fs.writeFile(path, text, "utf8", function (err, data) {
+                if (err) {
+                    reject(err); //Rejected promise will terminate the function
+                }
+                resolve(data);
+            });
+        });
+    },
+    "appendFile" : function (path, text) {
+        return new Promise(function (resolve, reject) {
+            fs.appendFile(path, text, "utf8", function (err, data) {
+                if (err) {
+                    reject(err); //Rejected promise will terminate the function
+                }
+                resolve(data);
+            });
+        });
+    }
 };
 
 
