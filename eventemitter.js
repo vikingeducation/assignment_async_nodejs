e2fb21537dd43eaeccb9528f9302dfa3b2c8f02e
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-20 01:31:17 +0000

    Add removeListern function

--- eventemitter.js
+++ eventemitter.js
@@ -20,7 +20,7 @@ function EventEmitter() {
             this.listeners[eventType] = [callback];
         }
         //if already a key, push callback onto the array
-        //else create new key for eventType with callBack as the value in an array.
+        //else create new key for eventType with callback as the value in an array.
         //reference to the emitter obj that also calls "on"
         
     };
@@ -31,7 +31,7 @@ function EventEmitter() {
     this.emit = function emit(eventType) {
         //Look up the eventType in emitter.listener obj
         let cbs = this.listeners[eventType];
-        if (cbs) {
+        if (Array.isArray(cbs) && cbs.length > 0) { //checks that there is an Array at this property and is not empty
             //call each function in the array
             cbs.forEach(function (element, index, arr) { //calls occur async
                 element();
@@ -41,18 +41,32 @@ function EventEmitter() {
     /** Removes a callback from the callbacks array of the given eventType
      * Will only remove one of the callbacks if multiples of the same callback are present
      * @params eventType [String] string of eventType
-     * @params callBack [Function] reference to the function to remove
+     * @params callback [Function] reference to the function to remove
      * @return undefined. Maybe make true or false depending on success, Wrap in promise?
      * */
     
     this.removeListener = function removeListener(eventType, callback) {
-        //See if eventType is in the listener property
-        //If so, see if there is a matching reference for the function passed in callback
-        //If so, remove it.
-        //If the array for this callback is now empty, remove the eventType property from the listeners
-        
-        
-        
+        let listeners = this.listeners[eventType]; //See if eventType is in the listener property
+
+        if (listeners) {//If so, loop through the array to see if there is a matching reference for the function passed in callback
+            //If so, remove it.
+            let i = 0;
+            while (i < listeners.length){
+                if (listeners[i] === callback) {//if matches, splice callback out, and break out of while
+                    listeners.splice(i, 1);
+                    break;
+                    //To allow for removing all callbacks of callback reference, do not break and do not increment i before continuing the loop
+                }
+                else {
+                    i++;
+                }
+            }
+            //If the array for this callback is now empty, remove the eventType property from the listeners
+            if (listeners.length === 0) {
+                delete this.listeners[eventType];
+            }
+        }
+        //If there are no listeners of property eventType, then finish
     };
     /** Deletes an eventType property from a listeners object
      * @params eventType [String] event type to remove
