ac34740a82e7dcffc83f75e863ce89adf8fe5c30
Ann Allan <ann.cristina.allan@gmail.com>
2017-03-11 23:17:45 -0500

    Add warmup 3 and 4 solutions

--- index.js
+++ index.js
@@ -8,3 +8,58 @@ p.then(function (message) {
 		console.log(message);
 	}, 1000);
 });
+
+// delay(milliseconds)
+function delay (milliseconds) {
+	setTimeout(function () {
+		var p = new Promise(function (resolve, reject) {
+			resolve(milliseconds - 100);
+			reject('error');
+		});
+	}, milliseconds);
+	console.log(milliseconds);
+	return p;
+}
+function countDown () {
+	delay();
+}
+delay(1000)
+ .then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown)
+	.then(countDown);
+
+// warmup 3
+function squared (number) {
+	var p = new Promise(function (resolve, reject) {
+		resolve(number * number);
+		reject('error');
+	});
+}
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+nums = nums.map(function (i) {
+	return Promise.resolve(squared(i));
+});
+Promise.all(nums)
+	.then(function (results) {
+		console.log(results);
+	});
+
+// warmup 4
+function doBadThing (forRealz) {
+	var p = new Promise(function (resolve, reject) {
+		if (forRealz) {
+			reject('error');
+		} else {
+			resolve('Yay');
+		}
+	});
+}
+doBadThing(true);
+doBadThing(false);
