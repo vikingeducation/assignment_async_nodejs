872e17d10d6ba0c126581ee4f53817daadb21931
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-13 16:05:56 -0700

    Add preliminary tinkering for returning promises with delayed resolutions

--- async.js
+++ async.js
@@ -1,15 +1,46 @@
 
 var p = new Promise(function(resolve, reject) {
-  if (true) {
-    resolve('Hello Promise!');
-  }
-  else {
-    reject('Something is wrong..');
-  }
+  resolve('Hello Promise!');
 });
 
 p.then(function(message) {
   setTimeout(function() {
     console.log(message);
-  }, 5000);
+  }, 1000);
 });
+
+
+var delay = function(milliseconds) {
+  return new Promise(function(resolve) {
+      resolve(console.log(milliseconds));
+  })
+};
+
+delay.then()
+//
+// return new Promise(function(resolve) {
+//     resolve(function() {
+//         console.log('rrrr');
+//         return $timeout(function(){},100);
+//     }());
+// }
+
+
+var countdown = function () {
+//  delay(1000).resolve();
+  Promise.resolve("Something");
+};
+
+countdown();
+//
+// delay(5000)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown);
