09f1498a57f45d05330a462be53d6bf19a50a134
Adam Jazairi <adamjazairi@gmail.com>
2017-03-14 13:36:37 -0400

    Add file operations

---
+++ file_ops_test.js
@@ -0,0 +1,30 @@
+var fsp = require('./lib/fsp.js');
+
+fsp.readFile('./data/lorem.txt')
+  .then(function(data) {
+    // Outputs the file data
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+fsp.writeFile('./data/test.txt', 'Hello!')
+  .then(function(res) {
+    // Outputs the file data
+    // after writing
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+fsp.appendFile('./data/test.txt', 'Hello again!')
+  .then(function(res) {
+    // Outputs the file data
+    // after appending
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
