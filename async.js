a2a94e1702d98b3222a6c19d11fd205501051334
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-13 18:49:47 -0700

    Finish promises warmup of assignment

--- async.js
+++ async.js
@@ -1,46 +1,89 @@
+// 1.
 
-var p = new Promise(function(resolve, reject) {
-  resolve('Hello Promise!');
-});
+// var p = new Promise(function(resolve, reject) {
+//     resolve('Hello Promise!');
+// });
+//
+// p.then(function(message) {
+//   setTimeout(function() {
+//     console.log(message);
+//     }, 1000);
+// });
 
-p.then(function(message) {
-  setTimeout(function() {
-    console.log(message);
-  }, 1000);
-});
+// 2.
 
+// var delay = function (milliseconds) {
+//
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function(){
+//       resolve(milliseconds);
+//     }, 100);
+//   });
+//
+// };
+//
+// var countDown = function (currentCount) {
+//   console.log(currentCount);
+//   return currentCount - 100;
+// };
+//
+// delay(1000)
+// .then(countDown) //=> 1000
+// .then(countDown) //=> 900
+// .then(countDown) //=> 800
+// .then(countDown) //=> 700
+// .then(countDown) //=> 600
+// .then(countDown) //=> 500
+// .then(countDown) //=> 400
+// .then(countDown) //=> 300
+// .then(countDown) //=> 200
+// .then(countDown) //=> 100
+// .then(countDown); //=> Done!
 
-var delay = function(milliseconds) {
-  return new Promise(function(resolve) {
-      resolve(console.log(milliseconds));
+// 3.
+
+// var promiseSquare = function(number) {
+//   return new Promise(function(resolve, reject) {
+//     if (Number.isInteger(number)) {
+//       resolve(number * number);
+//     }
+//     else {
+//       reject("Not a number");
+//     }
+//   });
+// };
+//
+// var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//
+// nums = nums.map(function(i) {
+//   return promiseSquare(i);
+// });
+//
+// Promise.all(nums)
+//   .then(function(results) {
+//     console.log(results);
+//   });
+
+// 4.
+
+var doBadThing = function(forRealz) {
+  return new Promise(function(resolve, reject) {
+    if (!forRealz) {
+      resolve("Yay!");
+    }
+    else {
+      reject("Meh");
+    }
+  });
+};
+
+doBadThing(false)
+  .then(function(result) {
+    console.log(result); //meh
+    throw("But also:");
   })
-};
+  .catch(function(err) {
+    console.error(err); //yay
+  });
 
-delay.then()
-//
-// return new Promise(function(resolve) {
-//     resolve(function() {
-//         console.log('rrrr');
-//         return $timeout(function(){},100);
-//     }());
-// }
-
-
-var countdown = function () {
-//  delay(1000).resolve();
-  Promise.resolve("Something");
-};
-
-countdown();
-//
-// delay(5000)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown)
-//   .then(countDown);
+  // Throw happens after catch even if it's placed before
