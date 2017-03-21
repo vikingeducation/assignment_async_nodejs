7de06b1f300ad91f2a3a5d08d96bbb13c07a50ed
markmarkyesyes <markhahn545@gmail.com>
2017-03-20 22:48:49 -0500

    completed .on method

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-var p = Promise.resolve('hello');
+// var p = Promise.resolve('hello');
 
-p.then(function(message) {
-  setTimeout(function() {
-    console.log(message)
-  }, 1000)
-});
-//////////////////////////////////////
+// p.then(function(message) {
+//   setTimeout(function() {
+//     console.log(message)
+//   }, 1000)
+// });
+// //////////////////////////////////////
 
-// function delay(milliseconds) {
-//   return new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//       resolve(ms)
-//     }, milliseconds)
+// // function delay(milliseconds) {
+// //   return new Promise(function(resolve, reject) {
+// //     setTimeout(function() {
+// //       resolve(ms)
+// //     }, milliseconds)
+// //   });
+// // };
+
+// // function countDown(milliseconds) {
+// //   var counter = milliseconds
+// // }
+
+// // delay(1000)
+// //   .then(function(value) {
+// //   console.log(value);
+// // })
+
+// /////////////////////////////////////////
+
+// function square(num) {
+//   var p = new Promise(function(value, error) {
+//     if (num == NaN ) {
+//       error("not a number");
+//     } else {
+//       value(num*num);
+//     }
 //   });
+//   return p;
 // };
 
-// function countDown(milliseconds) {
-//   var counter = milliseconds
+// var nums = [1,2,3,4,5,6,7,8,9];
+
+// nums = nums.map(function(i) {
+//   return square(i);
+// });
+
+// Promise.all(nums)
+//   .then(function(results) {
+//     console.log(results);
+// });
+
+
+// //////////////////////////////////////////
+
+// function doBadThing(forRealz) {
+//   var p = new Promise(function(resolve, reject) {
+//     if (forRealz) {
+//       reject('reject the truth!!')
+//     } else {
+//       resolve('EMBRACE THE FALSEY!!!')
+//     }
+//   });
+//   return p;
 // }
 
-// delay(1000)
-//   .then(function(value) {
-//   console.log(value);
-// })
+// doBadThing("Heya")
+//   .then(function(resolve) {
+//     console.log(resolve)
+//   })
+//   .catch(function(reject) {
+//     console.error(reject);
+//   });
 
-/////////////////////////////////////////
+////////////////////////////////////////////
 
-function square(num) {
-  var p = new Promise(function(value, error) {
-    if (num == NaN ) {
-      error("not a number");
-    } else {
-      value(num*num);
-    }
-  });
-  return p;
-};
+var Emitter = require('./lib/my-emitter.js');
+var emitter = new Emitter
 
-var nums = [1,2,3,4,5,6,7,8,9];
-
-nums = nums.map(function(i) {
-  return square(i);
-});
-
-Promise.all(nums)
-  .then(function(results) {
-    console.log(results);
-});
-
-
-//////////////////////////////////////////
-
-function doBadThing(forRealz) {
-  var p = new Promise(function(resolve, reject) {
-    if (forRealz) {
-      reject('reject the truth!!')
-    } else {
-      resolve('EMBRACE THE FALSEY!!!')
-    }
-  });
-  return p;
-}
-
-doBadThing("Heya")
-  .then(function(resolve) {
-    console.log(resolve)
-  })
-  .catch(function(reject) {
-    console.error(reject);
-  });
+emitter.on("boobs", "things")
