8526e4579e437b5af1c7bc820e45a2a272ea7f97
markmarkyesyes <markhahn545@gmail.com>
2017-03-21 23:42:58 -0500

    Pushed even listeners for the same event into an array linked to the event, then iterate over the array in the emit function.

--- index.js
+++ index.js
@@ -1,74 +1,77 @@
-// var p = Promise.resolve('hello');
+var p = Promise.resolve('hello');
 
-// p.then(function(message) {
-//   setTimeout(function() {
-//     console.log(message)
-//   }, 1000)
-// });
-// //////////////////////////////////////
+p.then(function(message) {
+  setTimeout(function() {
+    console.log(message)
+  }, 1000)
+});
+//////////////////////////////////////
 
-// // function delay(milliseconds) {
-// //   return new Promise(function(resolve, reject) {
-// //     setTimeout(function() {
-// //       resolve(ms)
-// //     }, milliseconds)
-// //   });
-// // };
+function delay(milliseconds) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(milliseconds)
+    }, milliseconds)
+  });
+};
 
-// // function countDown(milliseconds) {
-// //   var counter = milliseconds
-// // }
+function countDown(milliseconds) {
+  console.log(milliseconds);
+  return milliseconds - 100
+}
 
-// // delay(1000)
-// //   .then(function(value) {
-// //   console.log(value);
-// // })
+delay(1000)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown);
 
-// /////////////////////////////////////////
+/////////////////////////////////////////
 
-// function square(num) {
-//   var p = new Promise(function(value, error) {
-//     if (num == NaN ) {
-//       error("not a number");
-//     } else {
-//       value(num*num);
-//     }
-//   });
-//   return p;
-// };
+function square(num) {
+  var p = new Promise(function(value, error) {
+    if (num == NaN ) {
+      error("not a number");
+    } else {
+      value(num*num);
+    }
+  });
+  return p;
+};
 
-// var nums = [1,2,3,4,5,6,7,8,9];
+var nums = [1,2,3,4,5,6,7,8,9];
 
-// nums = nums.map(function(i) {
-//   return square(i);
-// });
+nums = nums.map(function(i) {
+  return square(i);
+});
 
-// Promise.all(nums)
-//   .then(function(results) {
-//     console.log(results);
-// });
+Promise.all(nums)
+  .then(function(results) {
+    console.log(results);
+});
 
 
-// //////////////////////////////////////////
+//////////////////////////////////////////
 
-// function doBadThing(forRealz) {
-//   var p = new Promise(function(resolve, reject) {
-//     if (forRealz) {
-//       reject('reject the truth!!')
-//     } else {
-//       resolve('EMBRACE THE FALSEY!!!')
-//     }
-//   });
-//   return p;
-// }
+function doBadThing(forRealz) {
+  var p = new Promise(function(resolve, reject) {
+    if (forRealz) {
+      reject('reject the truth!!')
+    } else {
+      resolve('EMBRACE THE FALSEY!!!')
+    }
+  });
+  return p;
+}
 
-// doBadThing("Heya")
-//   .then(function(resolve) {
-//     console.log(resolve)
-//   })
-//   .catch(function(reject) {
-//     console.error(reject);
-//   });
+doBadThing("Heya")
+  .then(function(resolve) {
+    console.log(resolve)
+  })
+  .catch(function(reject) {
+    console.error(reject);
+  });
 
 ////////////////////////////////////////////
 
@@ -84,8 +87,12 @@ var f2 = function() {
 var emitter = new Emitter;
 
 emitter.on("thing", f1)
+emitter.on("thing", f1)
+emitter.on("thing", f1)
 emitter.on("stuff", f2)
 
+emitter.emit("thing")
 
-emitter.emit("thing").removeListener("thing", f1).emit("thing")
+
+// emitter.emit("thing").removeListener("thing", f1).emit("thing")
 
