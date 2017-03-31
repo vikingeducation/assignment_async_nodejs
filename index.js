ee84209c8d4a29297dae8011e9a0960026019154
rabGIT <rabGIT@users.noreply.github.com>
2017-03-31 12:21:57 -0700

    emitter complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var Emitter = require('./lib/emitter');
+var emitter = new Emitter();
 
+// some testing functions for emitter
+emitter.on('CLICK', function(){console.log("click");});
+emitter.on('DONKEY', function(){console.log("donkey");});
+emitter.on('MOUSE', function(){console.log("mouse");});
+emitter.on('CLICK', function(){console.log("click");});
+emitter.on('CLICK', function(){console.log("click");});
+emitter.on('CLICK', function(){console.log("click you");});
+console.log(emitter.eventArray);
+emitter.emit('CLICK');
+emitter.removeAllListeners('CLICK');
+console.log(emitter.eventArray);
