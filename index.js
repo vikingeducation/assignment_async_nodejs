29f4e1968701dd265c19cb9bc47ba419b18f5d2b
rabGIT <rabGIT@users.noreply.github.com>
2017-03-31 12:24:25 -0700

    testing with node events replacing my emitter - worked

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var Emitter = require('./lib/emitter');
+var Emitter = require('events');
 var emitter = new Emitter();
 
-// some testing functions for emitter
+// some testing functions for emitter now with built in - still works :-)
 emitter.on('CLICK', function(){console.log("click");});
 emitter.on('DONKEY', function(){console.log("donkey");});
 emitter.on('MOUSE', function(){console.log("mouse");});
 emitter.on('CLICK', function(){console.log("click");});
 emitter.on('CLICK', function(){console.log("click");});
 emitter.on('CLICK', function(){console.log("click you");});
-console.log(emitter.eventArray);
 emitter.emit('CLICK');
 emitter.removeAllListeners('CLICK');
-console.log(emitter.eventArray);
+emitter.emit('CLICK');
