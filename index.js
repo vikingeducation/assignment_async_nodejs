8b8aacc63969a7c4f4d8f545b670976b306dd7d4
Lorraine Chan <lchan912@gmail.com>
2017-04-24 21:52:12 -0700

    Add fS wrapper

--- index.js
+++ index.js
@@ -45,3 +45,72 @@ const doBadThings = forRealz => {
 }
 
 doBadThings(false).then(value => { throw Error }).catch(error => console.log('test'));
+
+const fs = require('fs');
+
+const fsp = {
+  readFile: function(filePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  },
+
+  writeFile: function(filePath, content) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filePath, content, (err) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve('sweeet');
+        }
+      });
+    });
+  },
+
+  appendFile: function(filePath, content) {
+    return new Promise((resolve, reject) => {
+      fs.appendFile(filePath, content, (err) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve('aaaaaaadded');
+        }
+      });
+    });
+  }
+}
+
+fsp.readFile('./data/lorem.txt')
+  .then(function(data) {
+    // Outputs the file data
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+fsp.writeFile('./data/test.txt', 'Hello!')
+  .then(function(res) {
+    // Outputs the file data
+    // after writing
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+fsp.appendFile('./data/test.txt', 'Hello again!')
+  .then(function(res) {
+    // Outputs the file data
+    // after appending
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
