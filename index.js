7d1f7556f3c9922697db3911d7dde27e0a48dfcd
Tyler Ketron <tketron@gmail.com>
2017-04-29 18:30:11 -0700

    completed warmup problems

---
+++ index.js
@@ -0,0 +1,94 @@
+// var p = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
+//     resolve('Hello Promise!')
+//   }, 1000);
+// });
+
+// p.then(function(message) {
+//   console.log(message);
+// });
+//
+// function delay(milliseconds) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       resolve(milliseconds)
+//     }, milliseconds);
+//   });
+// };
+//
+// function countDown(value) {
+//   if (value >= 100) {
+//     console.log(value);
+//     return delay(value-100);
+//   } else {
+//     console.log("Done!");
+//   };
+// };
+//
+// delay(1000)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown)
+//   .then(countDown);
+
+function squarePromise(n) {
+  var p = new Promise(function(resolve, reject) {
+    if (Number.isInteger(n)) {
+      resolve(n*n);
+    } else {
+      reject("Not a number!");
+    };
+  });
+  return p;
+}
+
+// squarePromise("Hi").then(function(message) {
+//   console.log(message);
+// }, function(err) {
+//   console.error(err);
+// });
+
+var nums = [1,2,3,4,5,6,7,8,9];
+numsSquared = nums.map(function(i) {
+  return squarePromise(i);
+});
+
+Promise.all(numsSquared)
+  .then(function(results) {
+    console.log(results);
+  });
+
+function doBadThing(forRealz) {
+  var p = new Promise(function(resolve, reject) {
+    if (forRealz) {
+      reject("Bad! Truthyyyyy");
+    } else {
+      resolve("Yay!");
+    }
+  });
+  return p;
+}
+
+doBadThing(true)
+  .then(function(result) {
+    console.log(result);
+    throw("Thrown true");
+  }, function(result) {
+    console.error(result);
+  });
+
+doBadThing(false)
+  .then(function(result) {
+    console.log(result);
+    throw("Thrown false");
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
