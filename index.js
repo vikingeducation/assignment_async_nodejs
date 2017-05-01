51b46ec6a267e93d8eb1bba93b21c7a44044d522
Tyler Ketron <tketron@gmail.com>
2017-05-01 11:34:32 -0700

    implemented promise wrapper for fs operation as fsp module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var fsp = require('./fsp');
+
 // var p = new Promise(function(resolve, reject) {
 //   setTimeout(function() {
 //     resolve('Hello Promise!')
@@ -37,57 +39,86 @@
 //   .then(countDown)
 //   .then(countDown)
 //   .then(countDown);
-
-function squarePromise(n) {
-  var p = new Promise(function(resolve, reject) {
-    if (Number.isInteger(n)) {
-      resolve(n*n);
-    } else {
-      reject("Not a number!");
-    };
-  });
-  return p;
-}
+//
+// function squarePromise(n) {
+//   var p = new Promise(function(resolve, reject) {
+//     if (Number.isInteger(n)) {
+//       resolve(n*n);
+//     } else {
+//       reject("Not a number!");
+//     };
+//   });
+//   return p;
+// }
 
 // squarePromise("Hi").then(function(message) {
 //   console.log(message);
 // }, function(err) {
 //   console.error(err);
 // });
+//
+// var nums = [1,2,3,4,5,6,7,8,9];
+// numsSquared = nums.map(function(i) {
+//   return squarePromise(i);
+// });
+//
+// Promise.all(numsSquared)
+//   .then(function(results) {
+//     console.log(results);
+//   });
+//
+// function doBadThing(forRealz) {
+//   var p = new Promise(function(resolve, reject) {
+//     if (forRealz) {
+//       reject("Bad! Truthyyyyy");
+//     } else {
+//       resolve("Yay!");
+//     }
+//   });
+//   return p;
+// }
+//
+// doBadThing(true)
+//   .then(function(result) {
+//     console.log(result);
+//     throw("Thrown true");
+//   }, function(result) {
+//     console.error(result);
+//   });
+//
+// doBadThing(false)
+//   .then(function(result) {
+//     console.log(result);
+//     throw("Thrown false");
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//   });
 
-var nums = [1,2,3,4,5,6,7,8,9];
-numsSquared = nums.map(function(i) {
-  return squarePromise(i);
-});
-
-Promise.all(numsSquared)
-  .then(function(results) {
-    console.log(results);
+fsp.readFile('./test.txt')
+  .then(function(data) {
+    // Outputs the file data
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error(err);
   });
 
-function doBadThing(forRealz) {
-  var p = new Promise(function(resolve, reject) {
-    if (forRealz) {
-      reject("Bad! Truthyyyyy");
-    } else {
-      resolve("Yay!");
-    }
-  });
-  return p;
-}
+  fsp.writeFile('./test2.txt', 'Hello!')
+    .then(function(data) {
+      // Outputs the file data
+      // after writing
+      console.log(data);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 
-doBadThing(true)
-  .then(function(result) {
-    console.log(result);
-    throw("Thrown true");
-  }, function(result) {
-    console.error(result);
-  });
-
-doBadThing(false)
-  .then(function(result) {
-    console.log(result);
-    throw("Thrown false");
+  fsp.appendFile('./test2.txt', 'Hello again!')
+  .then(function(res) {
+    // Outputs the file data
+    // after appending
+    console.log(res);
   })
   .catch(function(err) {
     console.error(err);
