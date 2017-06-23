cc277bcc76c07d8759cfcb232d4a044ef5bba7d6
Kara Thrash <dvdonato@gmail.com>
2017-06-23 17:11:48 -0400

    added delay function

--- async.js
+++ async.js
@@ -3,3 +3,21 @@ p.then(function(message) {
   console.log(message);
   return 'Hi';
 });
+
+var pDelay = Promise.resolve(1000);
+
+function delay(ms){
+
+   var start = new Date().getTime();
+   console.log(start);
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+console.log(end);
+pDelay.then(function(ms) {
+  console.log(ms);
+  return 'Hi';
+});
+}
+delay(1111);
