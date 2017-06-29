81562b76f4a294080f9c7b0057b33b9ee5896354
Benjamin Soung <soungbenjamin@gmail.com>
2017-06-28 21:10:37 -0400

    changed some comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var fsp = require('./lib/fsp');
-var fs = require('fs');
 
 // print after 1 sec
 let p = new Promise(function(resolve, reject) {
@@ -12,23 +11,6 @@ p.then(function(message) {
 	console.log(message);
 })
 
-/*
-Your delay function should return a promise that resolves the value milliseconds after delaying for the specified number of milliseconds
-Create a countDown function that uses the delay function such that the following chaining and output is possible:
-
-delay(1000)
-  .then(countDown) //=> 1000
-  .then(countDown) //=> 900
-  .then(countDown) //=> 800
-  .then(countDown) //=> 700
-  .then(countDown) //=> 600
-  .then(countDown) //=> 500
-  .then(countDown) //=> 400
-  .then(countDown) //=> 300
-  .then(countDown) //=> 200
-  .then(countDown) //=> 100
-  .then(countDown); //=> Done!
- */
 
 let delay = function(milliseconds) {
 	let promise = new Promise(function(resolve, reject) {
@@ -41,20 +23,18 @@ let delay = function(milliseconds) {
 }
 
 let countdown = function() {
-	delay(1000)
-		.then(function(msg) {
-			console.log(msg - 100);
-			return msg - 100;
-		})
+	// going to refer to solutions
 	
 }
 
-// delay(1000)
-// 	.then(countdown)
-// 	.then(countdown)
+delay(1000)
+	.then(function(res) {
+		console.log(res);
+	})
 
 
-// Promise.all and map on arrays
+
+Promise.all and map on arrays
 let numbers = function(n) {
 	return new Promise(function(resolve, reject) {
 		if (typeof n !== 'number') {
@@ -79,7 +59,7 @@ Promise.all(arr)
 	})
 
 
-// do bad things for realz function
+do bad things for realz function
 let doBadThing = function(forRealz) {
 	return new Promise(function(resolve, reject) {
 		if (!forRealz) {
