3e2b2b754b86cb7ff7ef17be7a7077e398bc048b
Will Timpson <will.timpson@gmail.com>
2017-07-03 10:02:39 -0400

    Emitter: registering and triggering

--- emitter.js
+++ emitter.js
@@ -2,23 +2,42 @@
 
 function Emitter() {
 
+  // store for events and their listeners
+  this.events = {}
+
+
+  // register a listener for events of type 'eventType' to trigger 'callback'
   this.on = function(eventType, callback) {
-    // register a listener for events of type 'eventType' to trigger 'callback'
+    // add our event if necessary
+    if (this.events[eventType] === undefined) {
+      this.events[eventType] = [];
+    }
+
+    // register listener callback
+    this.events[eventType].push(callback)
 
   };
 
+  // trigger callbacks for an event of type 'eventType'
   this.emit = function(eventType) {
-    // trigger callbacks for an event of type 'eventType'
+
+    let callbacks = this.events[eventType];
+
+    if (callbacks !== undefined) {
+      callbacks.forEach(function(callback) {
+        callback();
+      });
+    }
 
   };
 
+  // remove the given 'callback' from an event of type 'eventType'
   this.removeListener = function(eventType, callback) {
-    // remove the given 'callback' from an event of type 'eventType'
 
   };
 
+  // remove all callbacks from an event of type 'eventType'
   this.removeAllListeners = function(eventType) {
-    // remove all callbacks from an event of type 'eventType'
 
   };
 
