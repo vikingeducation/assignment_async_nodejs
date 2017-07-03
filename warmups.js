cde6de730b8554a9d3f18a00b96e6dd55b5189d0
Will Timpson <will.timpson@gmail.com>
2017-07-02 22:02:46 -0400

    delay(), countDown()

--- warmups.js
+++ warmups.js
@@ -13,3 +13,35 @@ myPromise.then(function(message) {
   console.log(message);
 });
 
+
+// delay(milliseconds)
+
+function delay(milliseconds) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve(milliseconds);
+    }, milliseconds);
+  })
+};
+
+function countDown(time) {
+  if (time) {
+    console.log(time);
+    return delay(time - 100);
+  } else {
+    console.log('Done!')
+  }
+}
+
+delay(1000)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown);
