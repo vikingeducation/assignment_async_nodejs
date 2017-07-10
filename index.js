f849cd4409b729b80becc51cf4169725da104e5f
Ian Halverson <halversoncode@gmail.com>
2017-07-10 15:04:14 -0400

    emitter is working good

--- index.js
+++ index.js
@@ -1,187 +1,34 @@
-var fsp = require('./lib/fsp')
-
-/*--
-var pro = new Promise(function(resolve, reject) {if (1===1) {resolve("Hello Promise");} else {reject("Goodbye Promise")}});
-
-pro.then(function(result) {
-  setTimeout(function(){console.log(result)}, 1000),
-  function(result) {
-    setTimeout(function(){console.log(result + "is bad")}, 1000)}
-  });
+var fsp = require('./lib/fsp');
+var Emitter = require('./lib/events');
 
 
-var delay = function(milli) {
-  return prom = new Promise(function(resolve, reject){
-    setTimeout(function() {
-      resolve(milli);
-      reject(milli);
-    }, milli)
-  })
-}
-
-var fun = function(result){
-  return new Promise(function(resolve, reject){
-    setTimeout(function(){
-      resolve(result -= 100);
-      reject(result -=100);
-    }, 100)
-  })
-}
-
-delay(1100)
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-  .then(function(result){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        console.log(result -= 100);
-        resolve(result);
-        reject(result);
-      }, 100)
-    })
-  })
-
-var num = function(i) {
-  return Promise.resolve(i*i);
-};
-
-var intNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-intNum = intNum.map(function(i){
-  return Promise.resolve(i*i);
-});
+var emitter = new Emitter();
 
 
-Promise.all(intNum)
-  .then(function(results) {
-  console.log(results);
-});
-
-
-
-function doBadThing(forRealz) {
-  return new Promise(function(resolve, reject) {
-    if (!(forRealz)) {
-      resolve("Yay!");
-    }
-    else {
-      reject("Booo!");
-    }
-  });
-}
-
-doBadThing(null)
-  .then(function(results) {
-    console.log(results);
-    throw "error!";
-  }, function(err){
-    console.log(err);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-  --*/
-/*--
-var fs = require('fs');
-
-fs.appendFile("ianstext.txt", "I've added to the file!", function(err) {
-  if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});
---*/
-
-fsp.appendFile("ianstext.txt", "appended using a Promise function!")
-.then(function(res) {
-  console.log(res);
+emitter.on("click", function(){
+  console.log("Clicked!");
 })
+
+emitter.on("mouseover", function(){
+  console.log("Moused?...over?");
+})
+
+emitter.on("double-click", function(){
+  console.log("Clicked... twice real quick!!!");
+})
+
+emitter.on("click", function(){
+  console.log("Clicked numba 2!");
+})
+
+emitter.on("click", function(){
+  console.log("Clicked numba 3!");
+})
+
+emitter.emit("click");
+emitter.emit("mouseover");
+emitter.emit("double-click");
+
+emitter.removeAllListeners("click");
+
+emitter.emit("click");
