95b58c563ddf412ed83fe5680f7689a7e2bccbc9
anne <richardson.ae@gmail.com>
2017-08-02 16:28:53 -0500

    Build out delayer function

--- promises.js
+++ promises.js
@@ -1,9 +1,35 @@
 var delayed = require('delayed');
-var p = Promise.resolve('Hello Promise!');
 
-p.then(function(message) {
-  delayed.delay(function(){
-    console.log(message);
-  }, 1000);
-});
+var delayer = function(milliseconds){
+  // var p = Promise.resolve(milliseconds);
+  var p = new Promise(function(resolve, reject) {
+    resolve(
+      delayed.delay(function(){
+        console.log(milliseconds);
+      }, milliseconds)
+    );
+  });
+
+};
+
+delayer(500);
+
+
+// var p = Promise.resolve('Hello Promise!');
+
+// p.then(function(message) {
+//   delayed.delay(function(){
+//     console.log(message);
+//   }, 1000);
+// });
+
+
+
+
+
+
+
+
+
+
 
