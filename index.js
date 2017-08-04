dd33eb7ada6509d9592f6412a0587cccbc23cdff
anne <richardson.ae@gmail.com>
2017-08-04 14:01:47 -0500

    Write fsp.appendFile promise

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var dataPath = './data/data.json';
 var textPath = './data/lorem.txt';
 var newText = "This is the new text."
 
+// readFile
 fsp.readFile(textPath)
   .then(function(data) {
     console.log(data);
@@ -12,6 +13,7 @@ fsp.readFile(textPath)
     console.error(err);
   });
 
+// writeFile
 fsp.writeFile('./data/test.txt', 'Hello!')
   .then(function(res) {
     console.log(res);
@@ -21,12 +23,12 @@ fsp.writeFile('./data/test.txt', 'Hello!')
     console.error(err);
   });
 
-// fsp.appendFile('./data/test.txt', 'Hello again!')
-//   .then(function(res) {
-//     // Outputs the file data
-//     // after appending
-//     console.log(res);
-//   })
-//   .catch(function(err) {
-//     console.error(err);
-//   });
+// appendFile
+fsp.appendFile('./data/test.txt', 'Hello again!')
+  .then(function(res) {
+    console.log(res);
+    // Outputs the file data after appending
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
