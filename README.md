ea7caf9f83facae124789b1d8ab240bbe1305f4d
Jared Gebel <jaredjgebel@gmail.com>
2017-09-16 17:50:13 -0400

    Complete event emitter

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # assignment_async_nodejs
 Async Node.js sprint
+Jared Gebel
+9/8/2017
+
+Promises, File Operations, and Event Emitters
+Promises
+index.js
+
+File Operations
+fspTest.js
+./data/lorem.txt
+./data/test.txt
+./lib/fsp.js
+
+Reverse Engineering Node.js Event Emitter
+./lib/emitter-mode.js
+emitter.js
--- emitterTest.js
+++ emitterTest.js
@@ -1,8 +1,27 @@
 var Emitter = require("./lib/emitter");
-var emitter = new Emitter();
+var emitter = Emitter;
 
 var print = function(str) {
   console.log(str);
 };
 
-emitter.on("click", print);
+emitter.on("click", function() {
+  console.log("click has fired");
+});
+
+emitter.on("pop", function() {
+  console.log("pop has fired");
+});
+
+emitter.emit("click");
+emitter.emit("pop");
+
+emitter.removeListener("click");
+
+emitter.emit("click");
+emitter.emit("pop");
+
+emitter.on("click", function() {
+  console.log("click has fired");
+});
+emitter.emit("click");
