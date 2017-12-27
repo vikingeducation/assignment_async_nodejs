e5deb3e87f0c0e2b1d618a6d5712b80cf7456271
ksc23 <ksc.main@gmail.com>
2017-12-27 06:17:26 -0800

    Add emitter and test

---
+++ testEvents.js
@@ -0,0 +1,25 @@
+//const Emitter = require('./emitter');
+const Emitter = require('events');
+
+const emitter = new Emitter();
+
+// Event listeners
+const betListener = () => console.log('Bet!');
+const betListener2 = () => console.log('Bet!Bet!');
+const msgListener = (msg) => console.log(msg);
+
+emitter.on( 'bet', betListener );
+
+emitter.on( 'bet', betListener2 );
+
+emitter.on( 'msg', msgListener);
+
+emitter.emit('bet');
+
+emitter.emit('msg', 'Whats Up!');
+
+//emitter.removeAllListeners('bet');
+//emitter.emit('bet');
+emitter.removeListener('msg', msgListener);
+
+emitter.emit('msg', 'Im a message!');
