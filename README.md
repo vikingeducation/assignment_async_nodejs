c3916a913e4eb16e71142fc421bb3aaaf8c12d71
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-24 18:45:13 -0400

    first excercise completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # assignment_async_nodejs
 
 by Brennan Fulmer
+
+Just some asynchronous Node.js practice exercises
--- index.js
+++ index.js
@@ -3,6 +3,30 @@
   cd Documents/Viking/Node/async
 */
 
+function paused() {
+  // empty on purpose
+}
+
+var greet = new Promise(function(resolve, reject) {
+  resolve("Hello Promise!");
+});
+
+greet.then(function(greeting) {
+  setTimeout(() => {
+    console.log(greeting);
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+
+console.log("I ran");
+
 
 
 // spacing
