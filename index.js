14735d59d5eeb37532e42fccb9c4c02977a28589
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-24 22:49:12 -0400

    completed second excercise

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
   cd Documents/Viking/Node/async
 */
 
-function paused() {
-  // empty on purpose
-}
-
+// create a promise that resolves "Hello Promise!" after one second using .then
 var greet = new Promise(function(resolve, reject) {
   resolve("Hello Promise!");
 });
@@ -17,15 +14,42 @@ greet.then(function(greeting) {
   }, 1000);
 });
 
+/* Create a delay(milliseconds) function that should return a promise that
+resolves the value milliseconds after delaying for the specified number of
+milliseconds */
+
+function delay(milliseconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (milliseconds > 0) {
+        console.log(milliseconds);
+        resolve(milliseconds);
+      } else {
+        console.log("Done!")
+      }
+    }, milliseconds);
+  });
+}
+
+function countDown(ms) {
+  return delay(ms - 100);
+}
+
+delay(1000)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown)
+  .then(countDown);
 
 
 
-
-
-
-
-
-console.log("I ran");
 
 
 
