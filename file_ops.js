4e7098d5435d32e230436fc67e22c533c261477a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-25 16:45:11 -0400

    added files, and tested out some fs methods

--- file_ops.js
+++ file_ops.js
@@ -2,3 +2,48 @@
 /*
   cd Documents/Viking/Node/async
 */
+
+const fs = require('fs');
+
+fs.readFile('dummy.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log('I read "' + data + '"');
+});
+
+fs.writeFile('dummy.txt', 'test', 'utf8', (err) => {
+  if (err) throw err;
+  console.log('file overwritten');
+});
+
+fs.appendFile('dummy.txt', 'example', 'utf8', (err) => {
+  if (err) throw err;
+  console.log('data appended');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// spacing
