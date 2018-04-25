bf4ab68d1ddcc1801cfa651cdfc4b180f7a1d339
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-25 01:21:18 -0400

    finished 4th excercise

--- warmup.js
+++ warmup.js
@@ -1,11 +1,5 @@
 
-/*
-  cd Documents/Viking/Node/async
-*/
-
-/*
-create a promise that resolves "Hello Promise!" after one second using .then
-
+// create a promise that resolves "Hello Promise!" after one second using .then
 var greet = new Promise(function(resolve, reject) {
   resolve("Hello Promise!");
 });
@@ -16,13 +10,9 @@ greet.then(function(greeting) {
   }, 1000);
 });
 
-*/
-
-/*
-Create a delay(milliseconds) function that should return a promise that
+/* Create a delay(milliseconds) function that should return a promise that
 resolves the value milliseconds after delaying for the specified number of
-milliseconds
-
+milliseconds */
 function delay(milliseconds) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -30,7 +20,8 @@ function delay(milliseconds) {
         console.log(milliseconds);
         resolve(milliseconds);
       } else {
-        console.log("Done!")
+        console.log("Done!");
+        // throw new Error("Done!");
       }
     }, milliseconds);
   });
@@ -52,53 +43,49 @@ delay(1000)
   .then(countDown)
   .then(countDown)
   .then(countDown);
-*/
 
-/*
-Create a function that accepts a number and returns a promise that resolves
+/* Create a function that accepts a number and returns a promise that resolves
 that number squared, reject if not passed a number, and map an array of
-numbers 1-9 to the function using promise.all
-
-var integers = [1,2,3,4,5,6,7,8,9];
+numbers 1-9 to the function using promise.all */
+var integers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function squared(number) {
-  var square = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (isNaN(number)) {
       reject("Argument wasn't a number");
     } else {
       resolve(number * number);
     }
   });
-  return square;
 }
 
 Promise.all(integers.map(squared)).then(results => {
   console.log(results);
 });
-*/
 
-// excercise four
+/* basically play with the differences between then with and without catch and
+vice versa */
+function doBadThing(forRealz) {
+  var baddest = new Promise((resolve, reject) => {
+    if (forRealz == false) {
+      resolve("Yay!");
+    } else if (forRealz == true) {
+      reject("Only false will resolve");
+    }
+  });
+  baddest.then(
+    (response) => {
+      console.log(response);
+    },
+    (broke) => {
+      console.log(broke);
+    }
+  );
+  baddest.catch(error => {
+    console.log(error);
+  });
+  return baddest;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// spacing
+doBadThing(true);
+doBadThing(false);
