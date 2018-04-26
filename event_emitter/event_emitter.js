393d5bbc13e37bfb32f6273004faf2823851252c
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-26 15:10:09 -0400

    redoing file structure to better organize seperate project parts and prepare for module creation

---
+++ event_emitter/event_emitter.js
@@ -0,0 +1,3 @@
+
+var Emitter = require('.../lib/emitter.js');
+var emitter = new Emitter();
--- file_operations/file_ops.js
+++ file_operations/file_ops.js
@@ -5,7 +5,7 @@
 
 const fs = require('fs');
 
-fs.readFile('dummy.txt', 'utf8', (err, data) => {
+/* fs.readFile('dummy.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log('I read "' + data + '"');
 });
@@ -18,8 +18,16 @@ fs.writeFile('dummy.txt', 'test', 'utf8', (err) => {
 fs.appendFile('dummy.txt', 'example', 'utf8', (err) => {
   if (err) throw err;
   console.log('data appended');
-});
+}); */
 
+/* fsp.readFile('dummy.txt')
+  .then(function(data) {
+    // Outputs the file data
+    console.log(data);
+  })
+  .catch(function(err) {
+    throw new Error(err);
+  }); */
 
 
 
