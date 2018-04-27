0f4c8d4ce52c87d4383e28505e64943c09656664
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-27 03:10:59 -0400

    custom event emitter working

--- event_emitter/event_emitter.js
+++ event_emitter/event_emitter.js
@@ -1,3 +1,57 @@
 
 var Emitter = require(`../lib/emitter.js`);
+// var Emitter = require('events');
 var emitter = new Emitter();
+
+function alpha() {
+  console.log(`alpha`);
+}
+
+function bravo() {
+  console.log(`bravo`);
+}
+
+function charlie() {
+  console.log(`charlie`);
+}
+
+emitter.on(`event`, alpha);
+
+emitter.on(`event_2`, alpha).on('event', bravo).on(`event`, charlie);
+
+emitter.getter();
+
+emitter.emit(`event`);
+
+emitter.emit(`event_2`);
+
+emitter.removeListener(`event`, alpha);
+
+emitter.getter();
+
+emitter.removeAllListeners(`event_2`);
+
+emitter.getter();
+
+emitter.emit(`event`);
+
+emitter.emit(`event_2`);
+/**/
+
+
+
+/*
+var fs = require('fs');
+var text = fs.createReadStream('../README.md');
+
+text.on('open', (value) => {
+  console.log(`
+  opened with value ${value}`);
+});
+
+process.on('exit', (code) => {
+  console.log(`
+  About to exit with code: ${code}
+  `);
+});
+*/
