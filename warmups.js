62e253934c5400310edd2d681b60a869314850f9
Megan Taylor <mtaylor@megantaylor.org>
2018-06-09 14:57:40 -0400

    warmups #2

--- warmups.js
+++ warmups.js
@@ -10,3 +10,32 @@ var helloPromise = new Promise(function(resolve, reject) {
 helloPromise.then(function(hello) {
   console.log(hello);
 });
+
+//2. Create a delay(milliseconds) function that should return a promise thatresolves the value milliseconds after delaying for the specified number of milliseconds
+function delay(milliseconds) {
+  return new Promise((resolve, reject) => {
+    if (milliseconds > 0) {
+      console.log(milliseconds);
+      resolve(milliseconds);
+    } else {
+      console.log(`Done!`);
+    }
+  });
+}
+
+function countDown(time) {
+  return delay(time - 100);
+}
+
+delay(1000)
+  .then(countDown) //=> 1000
+  .then(countDown) //=> 900
+  .then(countDown) //=> 800
+  .then(countDown) //=> 700
+  .then(countDown) //=> 600
+  .then(countDown) //=> 500
+  .then(countDown) //=> 400
+  .then(countDown) //=> 300
+  .then(countDown) //=> 200
+  .then(countDown) //=> 100
+  .then(countDown); //=> Done!
